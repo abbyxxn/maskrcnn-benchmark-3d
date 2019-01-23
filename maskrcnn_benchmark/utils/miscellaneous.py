1127062c2548ec6260901acb01a664c25bce0de3
abbyx <abby.xxn@outlook.com>
2019-01-23 22:43:23 +0800

    add tensorboardX support

--- maskrcnn_benchmark/utils/miscellaneous.py
+++ maskrcnn_benchmark/utils/miscellaneous.py
@@ -1,6 +1,8 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 import errno
 import os
+import socket
+from datetime import datetime
 
 
 def mkdir(path):
@@ -9,3 +11,31 @@ def mkdir(path):
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
+
+
+def get_run_name():
+    """ A unique name for each run """
+    return datetime.now().strftime(
+        '%b%d-%H-%M-%S') + '_' + socket.gethostname()
+
+
+def get_output_dir(output_dir, args, run_name):
+    """ Get root output directory for each run """
+    cfg_filename, _ = os.path.splitext(os.path.split(args.config_file)[1])
+    return os.path.join(output_dir, cfg_filename, run_name)
+
+
+def get_tensorboard_writer(local_rank, distributed, output_dir):
+    try:
+        from tensorboardX import SummaryWriter
+    except ImportError:
+        raise ImportError(
+            'To use tensorboard please install tensorboardX '
+            '[ pip install tensorflow tensorboardX ].'
+        )
+
+    if not distributed or (distributed and local_rank == 0):
+        summary_logger = SummaryWriter(output_dir)
+        return summary_logger
+    else:
+        return None
--- tools/train_net.py
+++ tools/train_net.py
@@ -5,28 +5,27 @@ Basic training script for PyTorch
 
 # Set up custom environment before nearly anything else is imported
 # NOTE: this should be the first import (no not reorder)
-from maskrcnn_benchmark.utils.env import setup_environment  # noqa F401 isort:skip
 
 import argparse
 import os
 
 import torch
+
 from maskrcnn_benchmark.config import cfg
 from maskrcnn_benchmark.data import make_data_loader
-from maskrcnn_benchmark.solver import make_lr_scheduler
-from maskrcnn_benchmark.solver import make_optimizer
 from maskrcnn_benchmark.engine.inference import inference
 from maskrcnn_benchmark.engine.trainer import do_train
 from maskrcnn_benchmark.modeling.detector import build_detection_model
+from maskrcnn_benchmark.solver import make_lr_scheduler
+from maskrcnn_benchmark.solver import make_optimizer
 from maskrcnn_benchmark.utils.checkpoint import DetectronCheckpointer
 from maskrcnn_benchmark.utils.collect_env import collect_env_info
 from maskrcnn_benchmark.utils.comm import synchronize, get_rank
-from maskrcnn_benchmark.utils.imports import import_file
 from maskrcnn_benchmark.utils.logger import setup_logger
-from maskrcnn_benchmark.utils.miscellaneous import mkdir
+from maskrcnn_benchmark.utils.miscellaneous import mkdir, get_run_name, get_output_dir, get_tensorboard_writer
 
 
-def train(cfg, local_rank, distributed):
+def train(cfg, local_rank, distributed, output_dir):
     model = build_detection_model(cfg)
     device = torch.device(cfg.MODEL.DEVICE)
     model.to(device)
@@ -35,7 +34,7 @@ def train(cfg, local_rank, distributed):
     scheduler = make_lr_scheduler(cfg, optimizer)
 
     if distributed:
-        model = torch.nn.parallel.DistributedDataParallel(
+        model = torch.nn.parallel.deprecated.DistributedDataParallel(
             model, device_ids=[local_rank], output_device=local_rank,
             # this should be removed if we update BatchNorm stats
             broadcast_buffers=False,
@@ -44,7 +43,7 @@ def train(cfg, local_rank, distributed):
     arguments = {}
     arguments["iteration"] = 0
 
-    output_dir = cfg.OUTPUT_DIR
+    # output_dir = cfg.OUTPUT_DIR
 
     save_to_disk = get_rank() == 0
     checkpointer = DetectronCheckpointer(
@@ -62,7 +61,11 @@ def train(cfg, local_rank, distributed):
 
     checkpoint_period = cfg.SOLVER.CHECKPOINT_PERIOD
 
+    if cfg.VIS.USE_TENSORBOARD:
+        summary_logger = get_tensorboard_writer(local_rank, distributed, output_dir)
+
     do_train(
+        cfg,
         model,
         data_loader,
         optimizer,
@@ -71,6 +74,7 @@ def train(cfg, local_rank, distributed):
         device,
         checkpoint_period,
         arguments,
+        summary_logger,
     )
 
     return model
@@ -83,19 +87,21 @@ def test(cfg, model, distributed):
     iou_types = ("bbox",)
     if cfg.MODEL.MASK_ON:
         iou_types = iou_types + ("segm",)
+    if cfg.MODEL.BOX3D_ON:
+        iou_types = iou_types + ("bbox3d",)
     output_folders = [None] * len(cfg.DATASETS.TEST)
-    dataset_names = cfg.DATASETS.TEST
     if cfg.OUTPUT_DIR:
+        dataset_names = cfg.DATASETS.TEST
         for idx, dataset_name in enumerate(dataset_names):
             output_folder = os.path.join(cfg.OUTPUT_DIR, "inference", dataset_name)
             mkdir(output_folder)
             output_folders[idx] = output_folder
     data_loaders_val = make_data_loader(cfg, is_train=False, is_distributed=distributed)
-    for output_folder, dataset_name, data_loader_val in zip(output_folders, dataset_names, data_loaders_val):
+    for output_folder, data_loader_val in zip(output_folders, data_loaders_val):
         inference(
+            cfg,
             model,
             data_loader_val,
-            dataset_name=dataset_name,
             iou_types=iou_types,
             box_only=cfg.MODEL.RPN_ONLY,
             device=cfg.MODEL.DEVICE,
@@ -135,8 +141,10 @@ def main():
     args.distributed = num_gpus > 1
 
     if args.distributed:
+        # os.environ['MASTER_ADDR'] = '127.0.0.1'
+        # os.environ['MASTER_PORT'] = '29500'
         torch.cuda.set_device(args.local_rank)
-        torch.distributed.init_process_group(
+        torch.distributed.deprecated.init_process_group(
             backend="nccl", init_method="env://"
         )
 
@@ -145,6 +153,8 @@ def main():
     cfg.freeze()
 
     output_dir = cfg.OUTPUT_DIR
+    run_name = get_run_name() + '_step'
+    output_dir = get_output_dir(output_dir, args, run_name)
     if output_dir:
         mkdir(output_dir)
 
@@ -161,7 +171,7 @@ def main():
         logger.info(config_str)
     logger.info("Running with config:\n{}".format(cfg))
 
-    model = train(cfg, args.local_rank, args.distributed)
+    model = train(cfg, args.local_rank, args.distributed, output_dir)
 
     if not args.skip_test:
         test(cfg, model, args.distributed)
